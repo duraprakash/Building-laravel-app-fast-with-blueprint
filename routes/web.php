636a88f00936c7b1aeb47d442074a890be24e26d
duraprakash <duraprakash141@gmail.com>
2024-03-22 11:28:38 +0545

    blueprint config

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('employee', 'EmployeeController');
+
+Route::get('employee/test', [App\Http\Controllers\employmentController::class, 'test']);
